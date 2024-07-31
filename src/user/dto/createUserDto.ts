7662a21a96e0c866515ab85206d840dec6aa0ee2
DCTP-BE <akpata.thelma@gmail.com>
2024-07-31 17:54:58 +0100

    dto update

--- src/user/dto/createUserDto.ts
+++ src/user/dto/createUserDto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsNumberString, IsString } from "class-validator";
+
 export class CreateUserDto {
-    name:string;
+    @IsString()
+        name:string;
+
+    @IsEmail()
     email:string;
+
+    @IsNumberString()
     phone:string;
 }
