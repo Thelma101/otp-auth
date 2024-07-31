7f0ec30283d8b43023623e29190ef90445473521
DCTP-BE <akpata.thelma@gmail.com>
2024-07-31 20:10:39 +0100

    app module update

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { UserModule } from './user/user.module';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { CommentModule } from './comment/comment.module';
+import { CommentService } from './comment/comment.service';
 
 @Module({
   imports: [UserModule, CommentModule],
   controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  providers: [AppService, UserService, CommentService],
 })
 export class AppModule {}
