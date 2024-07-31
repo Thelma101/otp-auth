fb0093f006b85bf2ec871dc5046c76849bc59eb9
DCTP-BE <akpata.thelma@gmail.com>
2024-07-31 17:10:43 +0100

    new email params

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { UserController } from '..no-spec/user/user.controller';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [UserModule],
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    @Get(':id')
+    findAll(@Param("id") id:string ) {
+        return ({
+            user: {
+                id: id,
+                name: "John Doe",
+                email: "john.doe@example.com"
+            }
+        })
+    }
+}
