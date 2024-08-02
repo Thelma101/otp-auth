17640111de2434e874334f376af5fc43ac3c2c5b
DCTP-BE <akpata.thelma@gmail.com>
2024-08-02 11:35:14 +0100

    docker pg update

--- ormconfig.ts
+++ ormconfig.ts
@@ -6,17 +6,28 @@ import { Topic } from "src/entities/topic.entity";
 dotenv.config();
 
 
+// const config: PostgresConnectionOptions = {
+//     type: 'postgres',
+//     host: process.env.DB_HOST,
+//     port: parseInt(process.env.DB_PORT),
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     entities: [User, Comment, Topic],
+//     synchronize: true
+// }
+
 const config: PostgresConnectionOptions = {
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'testdb',
     entities: [User, Comment, Topic],
     synchronize: true
 }
 
+console.log(process.env);
 
 export default config;
-
