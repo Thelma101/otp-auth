9fa198cf33a272cd7795d5b8f51efee548012435
DCTP-BE <akpata.thelma@gmail.com>
2024-08-02 10:20:01 +0100

    topic entity decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-// import { UserController } from './user/user.controller';
-// import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 import { CommentModule } from './comment/comment.module';
-// import { CommentService } from './comment/comment.service';
+import { CommentService } from './comment/comment.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import  config from 'ormconfig';
 
 @Module({
   imports: [UserModule, CommentModule, TypeOrmModule.forRoot(config)],
-  // controllers: [AppController, UserController],
-  controllers: [AppController],
-  // providers: [AppService, UserService, CommentService],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  // controllers: [AppController],
+  providers: [AppService, UserService, CommentService],
+  // providers: [AppService],
 })
 export class AppModule {}
