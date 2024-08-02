a166ed3f30b4590631eeba19d5db7ead2e629614
DCTP-BE <akpata.thelma@gmail.com>
2024-08-02 18:03:40 +0100

    userRepo InjectRepository Function

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { CreateUserDto } from './dto/createUserDto';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from './dto/createUserDto';
 import { UserService } from './user.service';
 import { CommentService } from 'src/comment/comment.service';
 
@@ -20,5 +20,10 @@ export class UserController {
     @Get(':id/comments')
     getUserComment(@Param('id') id: string) {
         return this.commentService.findUserComments(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() updateUserDto:UpdateUserDto) {
+        return this.userService.update(id, updateUserDto);
     }
 }
