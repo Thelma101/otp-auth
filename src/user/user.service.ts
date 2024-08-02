d3e6d39bbaed7fada18af9b3a0636c192f35af16
DCTP-BE <akpata.thelma@gmail.com>
2024-08-02 16:26:35 +0100

    typeOrm config update

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUserDto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/entities/user.entity';
 
 @Injectable()
 export class UserService {
 
-    findOne(id:string) {
-        return{
-            id:id,
+    constructor(@InjectRepository [User] private readonly userRepo: Repository<User>,) { }
+    findOne(id: string) {
+        return {
+            id: id,
         };
     }
 
-    create(createUserDto : CreateUserDto ) {
+    create(createUserDto: CreateUserDto) {
         return "the user has been created successfully";
         // return this.userService.create(createUserDto)
-    
+
+    }
 }
-}
- 
