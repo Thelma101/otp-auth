9e6b63fb796d96dcbfbc44c6745837615f48da7d
DCTP-BE <akpata.thelma@gmail.com>
2024-08-09 20:20:24 +0100

    comment section module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { UserModule } from './user/user.module';
 // import { UserController } from './user/user.controller';
 // import { UserService } from './user/user.service';
 import { CommentModule } from './comment/comment.module';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
 import  config from 'ormconfig';
 
